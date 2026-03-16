3549e201b42b6cb44136b5f9428ec3bdcdf360b9
mattiacolombomc <info@mattiacolombo.com>
2026-03-16 12:28:25 +0100

    fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
 const { FlatCompat } = require("@eslint/eslintrc");
-const path = require("path");
+const js = require("@eslint/js");
 
 require("@rushstack/eslint-patch/modern-module-resolution");
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 module.exports = [...compat.extends("@raycast/eslint-config")];
